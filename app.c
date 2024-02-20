ad58947a353cb66fdfec68eceb4e9061f334bf8a
Chitwan23 <singhchitwan08@gmail.com>
2024-02-20 14:45:14 +0530

    branch2 changes

--- app.c
+++ app.c
@@ -1,5 +1,21 @@
-#include<stdio.h>
+
+#include<stdio.h> 
+void namaste();
+void bonjour();
+
 int main(){
-    printf("hello");
-    return 0;
+char ch ;
+  printf("enter i for indian and f for french: ");
+  scanf("%c",&ch);
+  if (ch=='i')
+{ namaste();
 }
+else{
+    bonjour();
+}}
+void namaste(){
+    printf("Namaste");
+}
+void bonjour(){
+    printf("Hola"); //changes from branch2
+} 
